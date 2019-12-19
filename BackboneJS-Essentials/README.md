a5d3af309be8760c2a59425899c8eea52a15c65a
Tochukwu Nwachukwu <truetochukz@gmail.com>
2019-12-19 16:41:25 +0200

    Model Basics

--- BackboneJS-Essentials/README.md
+++ BackboneJS-Essentials/README.md
@@ -4,7 +4,7 @@ Backbone requires JQuery and optionally Underscore.
 Backbone is made up of the following five major tools.  
 * A class system
 * A Model class
-* A collection class
+* A Collection class
 * A View class
 * A Router class
 
@@ -18,3 +18,39 @@ The new keyword does four thing when used for a function:
 4. Sets the \__proto\__ of the new object it creates to the *prototype* property of the initialization function.   
 
 Every object in JavaScript has two special properties: *prototype* and \__proto\__.  
+
+
+## Chapter 3: Accessing Server Data with Models  
+Models offer three major advantages:  
+* Models use Backbone's class system  
+* Models allow other code to listen for and respond to changes in the _Model_ attributes  
+* Models simplify and encapsulate the logic used to communicate with the server   
+
+__Attributes versus Properties__  
+Backbone's Model class can have regular _properties_ and _atttributes_. Attributes are properties of the 'attributes' property.
+You should use an attribute to store data when:
+* the data is going to be synced to the server
+* you want other parts of your code to be able to listen for data changes
+If your data doesn't meet these requirements, it's best to store it as a regular JavaScript property.
+
+Any of the core persistent information that a Model class is designed to hold belongs in its attribute, while any information that is secondary or derived, or which is only designed to last until the user refreshes the page, should be stored as properties.k
+
+Models have several different events available for you to listen to.  
+__Model Events__  
+* _change_ for change of any attribute 
+* _change: attribute_ for change of the specified attribute
+* _destroy_ for when the model is destroyed
+* _request_ for the start of AJAX method of the model
+* _sync_ for complete of AJAX method of the model
+* _error_ for error of AJAX method of the model
+* _invalid_ for failure of validation of thre Model _save_ or _isValid_ method call
+
+__Collection Related Events__  
+* _add_ for when the modelis added to a collection
+* _remove_ for when the model is removed form a collection
+* _reset_ for when the collection the model belongs to is reset.
+
+__The all Event__
+* _all_ triggered in esponse to any of the other model events. 
+
+Every model has three moethod to interact with the server - _fetch_, _save_ and _destroy_.  
---
+++ BackboneJS-Essentials/chp3/model-basics/backbone-models.js
@@ -0,0 +1,145 @@
+/*
+ * Defining backbone Model's default attribute
+ */
+const Book = Backbone.Model.extend({
+  defaults: { title: 'Backbone Essential'},
+});
+const book = new Book();
+const titleAttribute = book.attributes.title;
+console.log('Title: ', titleAttribute); // Backbone Essential
+
+/*
+ * If a default attribute is an object then it is shared between different objects of the Model class
+ * This is because JavaScript objects are pased by reference.
+ */
+const Person = Backbone.Model.extend({
+ defaults: { user: { name: 'Tochukwu'}}
+});
+const person1 = new Person();
+const person2 = new Person();
+
+console.log(person1.attributes.user.name); // Tochukwu
+console.log(person2.attributes.user.name); // Tochukwu
+// You should never set an attribute directly like this though
+person1.attributes.user.name = 'Chucks'; 
+console.log(person2.attributes.user.name); // Chucks
+
+/*
+ * Adding properties direct to Model when it is initialized
+ * Each instance will have its own set of initialized properties.
+ */
+const User = Backbone.Model.extend({
+  initialize: function(user) {
+    this.username = user.username;
+    this.email = user.email;
+  }
+});
+
+const users = [
+  {
+    username: 'Julichichi', 
+    email: 'Julichichi@hotmail.com'
+  },
+  {
+    username: 'Starboy',
+    email: 'starboy202@yahoo.com'
+  }
+]
+const user1 = new User(users[0]);
+const user2 = new User(users[1]);
+
+console.log('User 1: ', user1.username); // Julichichi
+console.log('User 2: ', user2.email); // starboy202@yahoo.com
+console.log('User1 Attributes', user1.attributes); // {username: "Julichichi", email: "Julichichi@hotmail.com"}
+
+/*
+ * Setting Model attribute  using the set() method 
+ */
+user1.set('city', 'Lagos');
+console.log('User1 City: ', user1.city); // undefined
+console.log('user1 City', user1.attributes.city); //Lagos 
+
+/* Using the overloaded set method to set attribute */
+user2.set({city: 'Lagos', car: 'Honder'});
+console.log('User2 Car:', user2.car); // undefined
+console.log('User2 Car:', user2.attributes.car); // Honder 
+
+
+/* 
+ * Unsetting Model attribute using the unset() method 
+ * The unset() method lets any code listening to the Model know about the change
+ */
+console.log('User2 City: ', user2.attributes.city); // Lagos
+/* use the unset method, don't use JavaScript delete operation i.e delete user2.attributes.city */
+user2.unset('city'); 
+console.log('User2 City: ', user2.attributes.city); // undefined 
+
+/*
+ * Use the Model get() method to retreive attribute. Don't do it directly like users2.attributes.car
+ */
+const user2Car = user2.get('car');
+console.log('User2 Car: ', user2Car); // Honder
+
+/*
+ * Listening for Model events using on() method
+ */
+const dev = new  Backbone.Model({
+  name: 'Tochukwu',
+  city: 'Joburg'
+});
+console.log('Dev Name: ', dev.get('name')); // Tochukwu;
+console.log('Dev City', dev.get('city')); // Joburg
+/* Do not access attributes like this. Use the get() method instead */
+// console.log('Dev name',  dev.attributes.name); // Tochukwu
+
+/* adding event listener for the city property */
+dev.on('change:city', function() {
+  console.log(`dev.city changed to: ${dev.get('city')}`);
+});
+dev.set('city', 'Cape Town');
+dev.set('city', 'Boston');
+/* removing event listening for the city property */
+dev.off('change:city');
+dev.set('city', 'Tokyo');
+
+/*
+ * Listening to multiple events on a Model by seperating the events with spaces 
+ */
+const laptop = new Backbone.Model({
+  name: 'HP'
+});
+laptop.on('change destroy', function(model) {
+console.log("Model: ", model);
+ if (model.attributes.name !== model._previousAttributes.name && model._previousAttributes.name !== undefined) {
+    console.log(`Name changed: ${model._previousAttributes.name} -> ${model.attributes.name}`); 
+ }
+});
+laptop.set('name', 'Lenovo'); // Name changed: HP -> Lenovo
+laptop.destroy(); // Name changed: HP -> Lenovo
+
+/* The on() method may take an optional third argument, an object argument, which it binds to the call back function to. You can pass this to it */
+const Tech = Backbone.Model.extend({
+  listenForLang: function(lang) {
+    lang.on('destroy', function() {
+      console.log(`Language: ${this.get('name')}, Level: ${this.get('level')}`);
+      console.log(`Language: ${lang.get('name')}, Level: ${lang.get('level')}`);
+    }, this); // "this" is out "context" argument
+  }
+})
+const java = new Tech({name: 'Java', level: 1});
+const cSharp = new Tech({name: 'C#', level: 3});
+java.listenForLang(cSharp);
+cSharp.destroy(); // Language: Java, Level: 1  \n Language: C#, Level: 3  
+
+/*
+ * Custom events 
+ */
+const Monitor = Backbone.Model.extend();
+const monitor = new Monitor({name: 'Samsung'});
+console.log('Monitor name:', monitor.get('name')); // Monitor name: Samsung
+monitor.on('brighten', function(brightness) {
+  console.log('Monitor Brightness: ', brightness);
+});
+/* trigger a custom function using the model trigger() method */
+monitor.trigger('brighten', 10); // Monitor Brightness: 10 
+

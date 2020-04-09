816be2021f10907fae530f79bafdb3950fee62f1
Chucks Nwachukwu <chucks@bbd.co.za>
2020-04-10 01:36:44 +0200

    Object-Oriented JavaScript with Backbone Classes

--- BackboneJS-Essentials/chp2/begin-underscore.js
+++ BackboneJS-Essentials/chp2/begin-underscore.js
@@ -1,28 +1,28 @@
 /*
- * window.setTImeout changes the 'this' context of its callback to the global window object
+ * window.setTimeout changes the 'this' context of its callback to the global window object
  */
 const myCallback = function() {
     console.log(this);
 }
 window.setTimeout(myCallback); // Window {parent: Window}
-/* jQuery event callbacks will change 'this' to the element that triggered the event 
- * jQuery AJAX callbacks will set it to the HTTP request created by the AJAX call
+/* jQuery event callbacks will change 'this' to the element that triggered the event
+ * jQuery AJAX callbacks will set 'this' to the HTTP request created by the AJAX call
  */
 
- /*
-  * using underscore bind 'this' to function
-  */
- const simpleBook = {};
+/*
+ * using underscore bind() function to bind 'this' to function: _.bind(func, obj)
+ */
+const simpleBook = {};
 simpleBook.logThis = function() {
     console.log(this)
 }
 
 window.setTimeout(simpleBook.logThis); // Window {parent: Window}
 simpleBook.logThis = _.bind(simpleBook.logThis, simpleBook);
-window.setTimeout(simpleBook.logThis); //{logThis: f}  
+window.setTimeout(simpleBook.logThis); //{logThis: f}
 
 /*
- * Using bindAll() to parmanently bind a method to an object 
+ * Using bindAll() to parmanently bind a method to an object
  */
 const Pencil = Backbone.Model.extend({
     initialize: function() {
@@ -35,8 +35,8 @@ const Pencil = Backbone.Model.extend({
 const pen = new Pencil();
 window.setTimeout(pen.logThis); // child {}
 /*
- * bindAll() allows you to use your class's methods with setTimeout or as classback to jQuery event handlers or AJAX operations , without losing 'this'.
- * bindAll shoudl not be overuse becuase it creates a new copy of every method it binds.
+ * bindAll() allows you to use your class's methods with setTimeout or as call-back to jQuery event handlers or AJAX operations , without losing 'this'.
+ * bindAll should not be overused because it creates a new copy of every method it binds.
  */
 
 /*
@@ -57,7 +57,7 @@ const scoresPlus10 = _.map(scores, function(score, index) {
 console.log('Scores: ', scoresPlus10);
 
 /*
- * using Userscores' reduce() function 
+ * using Userscores' reduce() function
  */
 const totalScore =  _.reduce(scores, function(total, score){
   return total + score;
@@ -69,18 +69,17 @@ console.log('Total Score: ', totalScore);
  */
 const dev = {clientTool: 'JS, Bootstrap', serverLang: 'PHP', title: 'Web Developer'};
 const engr = {serverLang: 'C#, JS, PHP', title: 'Full Stack Developer', db: 'Mongo'};
-const combined = _.extend({}, dev, engr); 
+const combined = _.extend({}, dev, engr);
 console.log('Combined: ', combined); // {clientTool: "JS, Bootstrap", serverLang: "C#, JS, PHP", title: "Full Stack Developer", db: "Mongo"}
 
 /*
- * Underscore's defaults() function only copies over values that the obect doesn't already have. 
- * 
+ * Underscore's defaults() function only copies over values that the obect doesn't already have.
  */
-const preserve = _.defaults({}, dev, engr); 
+const preserve = _.defaults({}, dev, engr);
 console.log('Default: ', preserve) // {clientTool: "JS, Bootstrap", serverLang: "PHP", title: "Web Developer", db: "Mongo"}
 
 /*
- * with Userscore's pluck method you can extract a single property value from each member of an array  
+ * with Userscore's pluck method you can extract a single property value from each member of an array
  */
 const books = [
     {
@@ -103,8 +102,8 @@ const authors = _.pluck(books, 'title');
 console.log(authors); //  ["Backbone Essential", "Pro Angular6", "Practical Node.js"]
 
 /*
- * Underscore's invoke method calls the specified method name on each element of the array passed as forst arguement  
- * _.invoke(list, methodName, [arguments]). a list of optional arguments can be passed as 3rd, 4th etc and forwarded to the method 
+ * Underscore's invoke method calls the specified method name on each element of the array passed as first arguement
+ * _.invoke(list, methodName, [arguments]). a list of optional arguments can be passed as 3rd, 4th etc and forwarded to the method
  */
 const TechBook = function(book) {
     this.title = book.title;
@@ -123,4 +122,4 @@ const techBooks = [
 
 const techBooksWithLang = _.invoke(techBooks, 'addLanguage', 'JavaScript');
 console.log(techBooks); // Each object in the array will now have a lang: "Javascript" property.
-console.log(techBooksWithLang); // Same as techBooks because we returned this from the invoked function i.e addLanguage. 
+console.log(techBooksWithLang); // Same as techBooks because we returned this from the invoked function i.e addLanguage.
